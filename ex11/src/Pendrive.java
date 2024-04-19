7b007cf48aef69b42fd0b27ca34bf89ba9683ee8
Gabriel Miranda <57235972+ddank0@users.noreply.github.com>
2024-04-19 00:56:13 -0300

    Update Pendrive.java

--- ex11/src/Pendrive.java
+++ ex11/src/Pendrive.java
@@ -2,7 +2,7 @@ public class Pendrive {
     //ATRIBUTOS
     private String  marca;
     private int     capacidade;
-    private boolean comprimento, 
+    private double comprimento, 
                     largura,
                     profundidade,
                     peso,
@@ -17,43 +17,43 @@ public class Pendrive {
         if(capacidade > 0)
             this.capacidade = capacidade;
     }
-    public void setComprimento(boolean comprimento){
-        if(capacidade > 0)
+    public void setComprimento(double comprimento){
+        if(comprimento > 0)
             this.comprimento = comprimento;
     }
-    public void setLargura(boolean largura){
-        if(capacidade > 0)
+    public void setLargura(double largura){
+        if(largura > 0)
             this.largura = largura;
     }
-    public void setProfundidade(boolean profundidade){
-        if(capacidade > 0)
+    public void setProfundidade(double profundidade){
+        if(profundidade > 0)
             this.profundidade = profundidade;
     }
-    public void setPeso(boolean peso){
-        if(capacidade > 0)
+    public void setPeso(double peso){
+        if(peso > 0)
             this.peso = peso;
     }
-    public void setPreco(boolean preco){
-        if(capacidade > 0)
+    public void setPreco(double preco){
+        if(preco > 0)
             this.preco = preco;
     }
     //
     public String getMarca() {
         return this.marca;
     }
-    public boolean getComprimento() {
+    public double getComprimento() {
         return comprimento;
     }
-    public boolean getLargura() {
+    public double getLargura() {
         return largura;
     }
-    public boolean getProfundidade() {
+    public double getProfundidade() {
         return profundidade;
     }
-    public boolean getPeso() {
+    public double getPeso() {
         return peso;
     }
-    public boolean getPreco() {
+    public double getPreco() {
         return preco;
     }
     public int getCapacidade() {
